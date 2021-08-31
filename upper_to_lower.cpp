149bc5acea759f5c8c701634b8a1123a3b540848
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-08-31 10:22:14 +0530

    Add files via upload

---
+++ upper_to_lower.cpp
@@ -0,0 +1,24 @@
+    /*the main logic is to add 32 to the all letters as difference between uppercase and lower case alphabets is 32
+        that is fist A,B,C,D,E,F...X,Y,Z THEN a,b,c,d,e.....x,y,z */
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    int l;
+    cout<<"enter an string "<<endl;
+    getline(cin,s);
+    l=s.length();
+    //cout<<'a'-'A';=32
+   for(int i=0;i<l;i++)
+    {
+        if(s[i]>='A'&&s[i]<='Z')
+        {
+            s[i]=s[i]+32;
+        }
+    } 
+    cout<<"the lowercase  string is "<<s<<endl;
+    return 0;
+}
